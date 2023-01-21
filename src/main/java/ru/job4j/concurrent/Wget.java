4dd43c51bd64a37af5cadbda3406a623e1782bd2
apereslavtsev <pereslavtsev.anton@gmail.com>
2023-01-21 19:48:24 +0300

    4. Режим ожидания. [#231217]

--- src/main/java/ru/job4j/concurrent/Wget.java
+++ src/main/java/ru/job4j/concurrent/Wget.java
@@ -6,18 +6,18 @@ public class Wget {
 	public static void main(String[] args) {
 		Thread thread = new Thread(
 				() -> {
-					for (int i = 0; i < 100; i++) {
+					for (int i = 0; i < 100; i++) {						
 						System.out.print("\rLoading : " + i  + "%");
+						try {
+							Thread.sleep(100);
+						} catch (InterruptedException e) {							
+							e.printStackTrace();
+						}
 					}
 				}
 		);
 		
 		thread.start();
-		try {
-			thread.currentThread().sleep(100);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
 		
 	}
 
