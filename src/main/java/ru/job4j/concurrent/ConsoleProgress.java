69ed0380e37c195277f05bae8b402efe459d8690
apereslavtsev <pereslavtsev.anton@gmail.com>
2023-01-25 09:44:59 +0300

    5. Прерывание нити [#1019 #384429]

--- src/main/java/ru/job4j/concurrent/ConsoleProgress.java
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -2,28 +2,28 @@ package ru.job4j.concurrent;
 
 public class ConsoleProgress implements Runnable {
 
-	@Override
-	public void run() {
-		var process = new char[] {'-', '\\',  '|', '/'};
-		int i = 0;
-		while (!Thread.currentThread().isInterrupted()) {
-			if (i >= process.length) {
-				i = 0;
-			}
-			try {
-				Thread.sleep(500);
-			} catch (InterruptedException e) {
-				Thread.currentThread().interrupt();				
-			}			
-			System.out.print("\r load: " + process[i++]);
-		}
-	}
-	
-	public static void main(String[] args) throws InterruptedException {
+    @Override
+    public void run() {
+        var process = new char[]{'-', '\\', '|', '/'};
+        int i = 0;
+        while (!Thread.currentThread().isInterrupted()) {
+            if (i >= process.length) {
+                i = 0;
+            }
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.print("\r load: " + process[i++]);
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
         Thread progress = new Thread(new ConsoleProgress());
         progress.start();
         Thread.sleep(5000); /* симулируем выполнение параллельной задачи в течение 5 секунд. */
         progress.interrupt();
-	}	
+    }
 
 }
