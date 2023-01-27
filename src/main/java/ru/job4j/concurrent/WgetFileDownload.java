dc87d7d856b438cbac2fb1ccf5aa958d96e5f147
apereslavtsev <pereslavtsev.anton@gmail.com>
2023-01-27 16:12:44 +0300

    8. Скачивание файла с ограничением. [#144271 #384817]

--- src/main/java/ru/job4j/concurrent/WgetFileDownload.java
+++ src/main/java/ru/job4j/concurrent/WgetFileDownload.java
@@ -10,9 +10,11 @@ import java.util.Calendar;
 public class WgetFileDownload implements Runnable {
 	private final String url;
 	private final int speed;
+	private final String target;
 
-	public WgetFileDownload(String url, int speed) {
+	public WgetFileDownload(String url, String target, int speed) {
 		this.url = url;
+		this.target = target;
 		this.speed = speed;
 	}
 
@@ -20,15 +22,21 @@ public class WgetFileDownload implements Runnable {
 	public void run() {
 		String file = url;
 		try (BufferedInputStream in = new BufferedInputStream(new URL(file).openStream());
-				FileOutputStream fileOutputStream = new FileOutputStream("pom_tmp.xml")) {
+				FileOutputStream fileOutputStream = new FileOutputStream(target)) {
+			int downloadData = 0;
 			byte[] dataBuffer = new byte[1024];
 			int bytesRead;
+			Long startTime = Calendar.getInstance().getTimeInMillis();
 			while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
-				Long startDate = Calendar.getInstance().getTimeInMillis();
 				fileOutputStream.write(dataBuffer, 0, bytesRead);
-				Long downloadTime = Calendar.getInstance().getTimeInMillis() - startDate;
-				if (speed > downloadTime) {
-					Thread.sleep(speed - downloadTime);
+				downloadData += bytesRead;
+				if (speed <= downloadData) {
+					Long currentTime = Calendar.getInstance().getTimeInMillis();
+					if (1000 > (currentTime - startTime)) {
+						Thread.sleep(1000 - (currentTime - startTime));
+					}
+					startTime = Calendar.getInstance().getTimeInMillis();
+					downloadData = 0;
 				}
 			}
 		} catch (IOException | InterruptedException e) {
@@ -37,14 +45,18 @@ public class WgetFileDownload implements Runnable {
 	}
 
 	public static void main(String[] args) throws InterruptedException, IOException {
+		if (args.length < 3) {
+			throw new IllegalArgumentException("The program expects 3 arguments: url, target, speed");
+		}
 		String sUrl = args[0];
 		URL url = new URL(sUrl);
 		HttpURLConnection huc = (HttpURLConnection) url.openConnection();
 		if (huc.getResponseCode() != HttpURLConnection.HTTP_OK) {
 			throw new IllegalArgumentException("url: " + url + " does not exist");
 		}
-		int speed = Integer.parseInt(args[1]);
-		Thread wget = new Thread(new WgetFileDownload(sUrl, speed));
+		String target = args[1];
+		int speed = Integer.parseInt(args[2]);
+		Thread wget = new Thread(new WgetFileDownload(sUrl, target, speed));
 		wget.start();
 		wget.join();
 	}
