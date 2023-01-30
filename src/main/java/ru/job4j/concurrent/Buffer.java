1eadc3d06535614d6a669edc44391bafc8a00057
apereslavtsev <pereslavtsev.anton@gmail.com>
2023-01-30 13:21:53 +0300

    0. Монитор, мьютекс, критическая секция [#6857 #386174]

--- src/main/java/ru/job4j/concurrent/Buffer.java
+++ src/main/java/ru/job4j/concurrent/Buffer.java
@@ -1,36 +1,34 @@
 package ru.job4j.concurrent;
 
 public class Buffer {
-    
+
     private StringBuilder buffer = new StringBuilder();
 
     /**
-     *  synchronized - монитор 
-     *  код метода - критическая секция
+     * объект класса - монитор код метода - критическая секция
      */
     public synchronized void add(int value) {
-        /*код метода - критическая секция*/
+        /* код метода - критическая секция */
         System.out.print(value);
         buffer.append(value);
     }
 
     /**
-     *  synchronized - монитор 
-     *  код метода - критическая секция
+     * объект класса - монитор код метода - критическая секция
      */
-    @Override   
+    @Override
     public synchronized String toString() {
-        /*код метода - критическая секция*/
+        /* код метода - критическая секция */
         return buffer.toString();
     }
-    
+
     public void sout() {
         if (!buffer.isEmpty()) {
-            /*synchronized(this) - монитор*/
-            synchronized(this) {
-                /*код внутри блока synchronized(this) - критическая секция*/
+            /* this - объект класса - монитор */
+            synchronized (this) {
+                /* код внутри блока synchronized(this) - критическая секция */
                 System.out.println(toString());
-            }            
+            }
         }
     }
 
