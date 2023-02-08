cbb2539df0173a00bf34a24830bd477d4de4b26d
apereslavtsev <pereslavtsev.anton@gmail.com>
2023-02-08 16:22:33 +0300

    0. CAS - операции [#6859]

---
+++ src/main/java/ru/job4j/concurrent/Cas/CASCount.java
@@ -0,0 +1,24 @@
+package ru.job4j.concurrent.Cas;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import net.jcip.annotations.ThreadSafe;
+
+@ThreadSafe
+public class CASCount {
+    
+    private final AtomicInteger count;
+    
+    public CASCount(int count) {
+        this.count = new AtomicInteger(count);
+    }
+
+    public void increment() {
+        count.incrementAndGet();
+    }
+
+    public int get() {
+        return count.get();
+    }
+
+}
---
+++ src/test/java/ru/job4j/concurrent/Cas/CASCountTest.java
@@ -0,0 +1,37 @@
+package ru.job4j.concurrent.Cas;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+class CASCountTest {
+
+    @Test
+    void when10000ThreadsThenCountIs100mln() throws InterruptedException {
+        CASCount count = new CASCount(0); 
+        List<Thread> threads = new ArrayList<>();
+        for (int i = 0; i < 10000; i++) {
+            threads.add(new Thread(() -> 
+            {
+                for (int j = 0; j < 10000; j++) {
+                    count.increment();
+                }
+            }));            
+        }
+        
+        for (Thread thread : threads) {
+            thread.start();
+        }
+        
+        for (Thread thread : threads) {
+            thread.join();
+        }
+        assertThat(threads.size()).isEqualTo(10000);
+        assertThat(count.get()).isEqualTo(100000000);
+    }
+
+
+}
